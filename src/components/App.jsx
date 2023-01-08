211827c9ee88769f7f612bb5e0d5df10a2c83877
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-01-08 21:12:48 +0200

    added loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { GlobalStyle } from './GlobalStyle';
 import { useEffect } from 'react';
 import { fetchContacts } from 'Redux/operations';
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -12,14 +13,15 @@ export const App = () => {
     dispatch(fetchContacts());
   }, [dispatch]);
   const contacts = useSelector(state => state.contacts.items);
+  const loading = useSelector(state => state.contacts.isLoading);
 
   return (
     <div>
       <GlobalStyle />
       <h1>PhoneBook</h1>
       <ContactForm />
-      <h2>Contacts :</h2>
-      {contacts.length === 0 ? (
+      <h2>Contacts : {loading && <Loader />} </h2>
+      {contacts.length === 0 && !loading ? (
         <h2>You have no contacts saved</h2>
       ) : (
         <>
